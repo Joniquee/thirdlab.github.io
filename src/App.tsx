cae53458e5b6c7816f28b3bfc699fd9b248d63b7
Joniq <ahnychkov@gmail.com>
2025-12-15 20:06:25 +0300

    some ch

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,22 @@ import { NavLink, Routes, Route } from "react-router-dom";
 import Cats from "./pages/Cats";
 import Crypto from "./pages/Crypto";
 import News from "./pages/News";
+import './styles.css'
 
 export default function App() {
   return (
     <>
       <nav>
-        <NavLink to="/">Новости</NavLink>
-        <NavLink to="/cats">Коты</NavLink>
-        <NavLink to="/crypto">Крипта</NavLink>
+	<NavLink to="/thirdlab.github.io">Главная</NavLink>
+        <NavLink to="/thirdlab.github.io/news">Новости</NavLink>
+        <NavLink to="/thirdlab.github.io/cats">Коты</NavLink>
+        <NavLink to="/thirdlab.github.io/crypto">Крипта</NavLink>
       </nav>
 
       <Routes>
-        <Route path="/" element={<News />} />
-        <Route path="/cats" element={<Cats />} />
-        <Route path="/crypto" element={<Crypto />} />
+        <Route path="/thirdlab.github.io/news" element={<News />} />
+        <Route path="/thirdlab.github.io/cats" element={<Cats />} />
+        <Route path="/thirdlab.github.io/crypto" element={<Crypto />} />
       </Routes>
     </>
   );
